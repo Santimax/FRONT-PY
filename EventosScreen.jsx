bac8815577653e698f3072eb19fda9f5e8332018
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-08 12:07:04 -0300

    Fin

--- EventosScreen.jsx
+++ EventosScreen.jsx
@@ -1,20 +1,72 @@
-import * as React from 'react';
-import { ActivityIndicator, FlatList, Text, View, Button } from 'react-native';
-import Axios from 'axios';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { useNavigation } from '@react-navigation/native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, ActivityIndicator, FlatList, SafeAreaView,} from 'react-native';
+import axios from 'axios';
 
-const EventosScreen = () =>{
-    const navigationHook = useNavigation();    
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Eventos</Text>
-        <Button
-            title="Go to Home"
-            onPress={() => navigationHook.navigate('Home')}
-        />
-        </View>
-    );
-}
-export default EventosScreen
+    const EventosScreen = () => {
+        const [data, setData] = useState(null);
+        const [loading, setLoading] = useState(true);
+        const [error, setError] = useState(null);
+
+        useEffect(() => {
+            const fetchData = async () => {
+            try {
+            const response = await axios.get(
+                'http://a-phz2-cidi-045:3000/API/eventos',
+            );
+            setData(response.data);
+            setLoading(false);
+            } catch (e) {
+            setError(e);
+            setLoading(false);
+        }
+        };
+
+        fetchData();
+        }, []);
+
+        if (loading) {
+        return (
+            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <ActivityIndicator />
+            </View>
+        );
+        }
+
+        if (error) {
+        return (
+            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text>An error occurred</Text>
+            </View>
+        );
+        }
+
+        const renderItem = ({item}) => {
+            return (
+                <View
+                style={{
+                padding: 10,
+                marginVertical: 8,
+                marginHorizontal: 16,
+                backgroundColor: '#f9c2ff',
+            }}>
+            <Text style={{fontSize: 18}}>{item.idEvento}</Text>
+            <Text style={{fontSize: 14}}>{item.nombreEvento}</Text>
+            <Text style={{fontSize: 14}}>{item.descripcionEvento}</Text>
+            <Text style={{fontSize: 14}}>{item.fk_tipoEvento}</Text>
+            </View>
+        );
+    };
+
+        return (
+        <SafeAreaView
+            style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <FlatList
+            data={data}
+            renderItem={renderItem}
+            keyExtractor={item => item.id}
+            />
+        </SafeAreaView>
+        );
+    };
+
+    export default EventosScreen;
