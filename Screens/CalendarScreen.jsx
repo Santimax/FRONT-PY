bbcf9b939069d493190298f2dfea89709b6236a4
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-10-27 10:24:12 -0300

    hola

--- Screens/CalendarScreen.jsx
+++ Screens/CalendarScreen.jsx
@@ -15,7 +15,7 @@ const CalendarScreen = () =>{
 
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home</Text>
+        <Text>Calendario</Text>
         <Button
             title="Eventos"
             onPress={handleClick}
